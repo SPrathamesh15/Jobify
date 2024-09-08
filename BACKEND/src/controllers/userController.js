1ef1af8aa5358dc37251815a0c16bd5e999cced6
SPrathamesh15 <sprathamesh354@gmail.com>
2024-09-08 22:47:56 +0530

    updated

--- BACKEND/src/controllers/userController.js
+++ BACKEND/src/controllers/userController.js
@@ -147,7 +147,7 @@ exports.verifyOtpAndSignup = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'none',
-      domain: process.env.NODE_ENV === 'production' ? '.vercel.app' : 'localhost',
+      domain: process.env.NODE_ENV === 'production' ? '.onrender.com' : 'localhost',
       path: '/',
       maxAge: 6 * 30 * 24 * 60 * 60 * 1000, // 6 months
     });
@@ -191,7 +191,7 @@ exports.login = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'none',
-      domain: process.env.NODE_ENV === 'production' ? '.vercel.app' : 'localhost',
+      domain: process.env.NODE_ENV === 'production' ? '.onrender.com' : 'localhost',
       path: '/',
       maxAge: 6 * 30 * 24 * 60 * 60 * 1000
     });
